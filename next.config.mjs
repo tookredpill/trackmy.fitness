7851edc26e693ff755a8748009f580c045f70c9b
tookredpill <tookredpill@tutanota.com>
2023-01-13 12:28:28 +0530

    chore: validate env variables on start

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,26 @@
+import { z } from "zod";
+
+const parseResult = z
+  .object({
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1)
+  })
+  .safeParse(process.env);
+
+if (!parseResult.success) {
+  console.error(
+    "❌ Invalid environment variables:\n",
+    ...Object.entries(parseResult.error.format())
+      .map(([name, value]) => {
+        if (value && "_errors" in value) {
+          return `${name}: ${value._errors.join(", ")}\n`;
+        }
+      })
+      .filter(Boolean)
+  );
+  throw new Error("Invalid environment variables");
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
