6443e8a8ffca03b2515f9273d44e26e116ba2a33
tookredpill <tookredpill@tutanota.com>
2023-01-17 21:34:52 +0530

    refactor: reorder .gitignore and environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -3,11 +3,11 @@ declare namespace NodeJS {
     DATABASE_URL: string;
     GOOGLE_CLIENT_ID: string;
     GOOGLE_CLIENT_SECRET: string;
-    // This will default to `VERCEL_URL` on deployment. Not needed in production.
+    // Set by vercel during deployment.
+    VERCEL_URL?: string;
+    // This will default to `VERCEL_URL` on deployment. Not used in production.
     NEXTAUTH_URL: string;
     // Must be set when running in production. Automatically generated during development.
     NEXTAUTH_SECRET?: string;
-    // Set by vercel during deployment.
-    VERCEL_URL?: string;
   }
 }
